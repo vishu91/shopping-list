11f80eaf8ce2f8322758a264beafdddc600c81da
bishwajit.gupta <12mcmc45@gmail.com>
2019-04-22 12:00:41 +0530

    Service Injection In angular

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,26 +8,15 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe 1', 'This is test recipe',
-      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg'),
-    new Recipe('Test Recipe 2', 'This is test recipe',
-      'https://www.bbcgoodfood.com/sites/default' +
-      '/files/recipe-collections/collection-image/2013/05/dinner-party-dessert-profiteroles.jpg'),
-    new Recipe('Test Recipe 3', 'This is test recipe',
-      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/' +
-      '2016/06/matcha-mousse-cake.jpg?itok=8epMXuWz')
-  ];
 
-  constructor() {
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,17 +10,18 @@ import {Ingredient} from '../../shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() {
+  constructor(private slService: ShoppingListService) {
   }
 
   ngOnInit() {
   }
 
   onAddItem() {
-    const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
-    this.ingredientAdded.emit(newIngredient);
+    const name = this.nameInputRef.nativeElement.value;
+    const amount =  this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(name, amount);
+    this.slService.addIngredient(newIngredient);
   }
 
 }
